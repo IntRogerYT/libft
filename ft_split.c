61d399e7923969bf7dbde3a0a596f042f3c325eb
Roger Camps Viader <rcamps-v@cbr6s5.42barcelona.com>
2025-10-25 14:54:31 +0200

    ft_split terminado

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,43 @@
 /*   By: rcamps-v <rcamps-v@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 13:14:43 by rcamps-v          #+#    #+#             */
-/*   Updated: 2025/10/23 14:18:28 by rcamps-v         ###   ########.fr       */
+/*   Updated: 2025/10/25 14:50:24 by rcamps-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_words_count(char const *s, char c)
+{
+	size_t	i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	if (*s == '\0')
+		return (0);
+	while (s[i] == c)
+		i++;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c && s[i - 1] != c)
+			count++;
+		i++;
+	}
+	if (s[i - 1] == c)
+		return (count);
+	return (count + 1);
+}
+
+void	ft_free(char **matrix, size_t aux)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < aux)
+		free(matrix[i++]);
+	free(matrix);
+}
 
 size_t	ft_startpos(char const *s, char c)
 {
@@ -23,27 +55,47 @@ size_t	ft_startpos(char const *s, char c)
 			return (i);
 		i++;
 	}
-	return (0);
+	return (i);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split_words(char **matrix, char const *s, char c, size_t words)
 {
-	char	**array;
 	size_t	len;
 	size_t	start;
 	size_t	i;
 
 	i = 0;
 	start = 0;
-	len = ft_strlen(s);
-	array = 0;
-	while (ft_startpos((s + start), c) != 0)
+	while (i < words)
 	{
+		while (s[start] == c)
+			start++;
 		len = ft_startpos((s + start), c);
-		array[i] = ft_substr(s, start, len);
-		start = len + 1;
+		matrix[i] = ft_substr(s, start, len);
+		if (!matrix[i])
+		{
+			ft_free(matrix, i);
+			return (NULL);
+		}
+		start += len + 1;
 		i++;
 	}
-	array[i] = "\0";
-	return (array);
+	matrix[i] = NULL;
+	return (matrix);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**matrix;
+	size_t	words;
+
+	if (!s)
+		return (NULL);
+	words = ft_words_count(s, c);
+	matrix = malloc((ft_words_count(s, c) + 1) * sizeof(char *));
+	if (!matrix)
+		return (NULL);
+	if (ft_split_words(matrix, s, c, words) == NULL)
+		return (NULL);
+	return (matrix);
 }
